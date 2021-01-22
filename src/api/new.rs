cb605692f5367bb889068d48b73b47b42f470b1d
baoyachi <liaoymxsdl@gmail.com>
2021-01-22 20:57:02 +0800

    fix

--- src/api/new.rs
+++ src/api/new.rs
@@ -1,3 +1,6 @@
+use futures_core::stream::Stream;
+use futures_util::StreamExt;
+
 // 2021-01-22 01:43:37:654200000 [INFO] <actix_server::builder:263>:Starting 4 workers
 // 2021-01-22 01:43:37:655028000 [INFO] <actix_server::builder:277>:Starting "actix-web-service-127.0.0.1:8080" service on 127.0.0.1:8080
 // 2021-01-22 01:49:13:270352000 [INFO] <pargo::api::server:11>:
@@ -14,9 +17,28 @@
 
 use actix_web::{HttpRequest, HttpResponse, put, web, HttpMessage};
 use crate::api::error::ApiResult;
+use std::fs;
+use std::io::Write;
+use std::path::Path;
 
 #[put("/api/v1/crates/new")]
-pub async fn new(mut req: HttpRequest) -> ApiResult<HttpResponse> {
-    info!("req:{:?}", req);
+pub async fn new(
+    req: HttpRequest,
+    mut payload: web::Payload,
+) -> ApiResult<HttpResponse> {
+    info!("{:?}",req);
+
+    let mut bytes = web::BytesMut::new();
+    while let Some(item) = payload.next().await {
+        bytes.extend_from_slice(&item.unwrap());
+    }
+
+    let path = Path::new("test_download/test.crates");
+    let mut file = fs::OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(&path).unwrap();
+    file.write_all(bytes.as_ref()).unwrap();
+
     Ok(HttpResponse::Ok().finish())
 }
--- src/api/server.rs
+++ src/api/server.rs
@@ -3,23 +3,27 @@ use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use crate::api::error::ApiResult;
 use actix_web::http::StatusCode;
 use crate::api::new::new;
+use actix_web::middleware::Logger;
 
 fn router(cfg: &mut web::ServiceConfig) {
-    cfg.service(download);
-    // .service(new);
+    cfg.service(download)
+    .service(new);
 }
 
 async fn not_found(req:HttpRequest) -> ApiResult<HttpResponse> {
-    info!("{:?}",req);
     Ok(HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
         .body("<h1>Error 404</h1>"))
 }
 
 pub async fn start_server() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "my_errors=debug,actix_web=info");
+    std::env::set_var("RUST_BACKTRACE", "1");
+
     HttpServer::new(||
         App::new()
             .configure(router)
+            .wrap(Logger::default())
             .default_service(web::route().to(not_found))
     )
         .bind(("127.0.0.1", 8080))?
